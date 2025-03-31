c14c96e73f2b834e52b397d91e6de1d3eb980119
Max Oberzaucher <oberzauchermax@gmail.com>
2025-03-31 23:03:39 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "tailwindcss";
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1 className="text-red-900">Meine erste React-Webseite</h1>
+      <p>Hallo Welt! 🎉</p>
+    </div>
+  );
+}
+export default App;
